aa740ffce4027ff52a760a86aae5b70c8de496ef
Wladson Cedraz <wladylinux@gmail.com>
2021-11-18 20:47:27 -0300

    Iniciando uso do Entity Framework Core 'dotnet tools install --global dotnet-ef'

--- DevReviews.API/Persistence/DevReviewsDbContext.cs
+++ DevReviews.API/Persistence/DevReviewsDbContext.cs
@@ -1,14 +1,35 @@
 using System.Collections.Generic;
 using DevReviews.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevReviews.API.Persistence
 {
-    public class DevReviewsDbContext
+    public class DevReviewsDbContext : DbContext
     {
-        public DevReviewsDbContext()
+        public DevReviewsDbContext(DbContextOptions<DevReviewsDbContext> options) : base(options)
         {
-            Products = new List<Product>();
+
         }
-        public List<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductReview> ProductReviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(p =>
+            {
+                p.ToTable("tb_Product");
+                p.HasKey(p => p.Id);
+            });
+
+            modelBuilder.Entity<ProductReview>(pr =>
+            {
+                pr.ToTable("tb_ProductReviews");
+                pr.HasKey(p => p.Id);
+
+                pr.Property(p => p.Author)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
     }
 }
